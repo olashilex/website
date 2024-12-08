1ea40258e302d11b76d8a22a95a555524bae3ff2
olashile <shilebalogun0507@gmail.com>
2024-12-08 15:35:16 +0100

    edited

--- demo/index.php
+++ demo/index.php
@@ -6,7 +6,7 @@ $uri = $_SERVER['REQUEST_URI'];
 if ($uri === '/demo/' || $uri === '/demo/index.php') {
     require "controllers/index.php";
 } else if ($uri === '/about/' || $uri === '/demo/about.php'){
-  require "about/index.php";
+  require "controllers/about.php";
 } else if ($uri === '/contact/' || $uri === '/demo/contact.php'){
     require "controllers/contact.php";
 }
